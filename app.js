d40e1cd2d35bf5c133760ad8e4588bd906fbad85
Narotam Das <narotamdas98@gmail.com>
2022-08-17 01:35:53 +0500

    databse

--- app.js
+++ app.js
@@ -3,11 +3,27 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const mysql = require("mysql");
+const cors = require("cors");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
+var mysqlConnection = mysql.createConnection({
+  host: "server.blackloophosters.com",
+  // host: "database-1.cfzvpgeuxa84.us-east-1.rds.amazonaws.com",
+  user: "sbkjapan_sbkjapanusr",
+  password: "Bodrum123@456",
+  database: "sbkjapan_db",
+  multipleStatements: true,
+  timeout: 3000,
+});
+app.use(cors());
+mysqlConnection.connect((err) => {
+  if (!err) console.log("Connection Established Successfully");
+  else console.log("Connection Failed!" + JSON.stringify(err, undefined, 2));
+});
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
